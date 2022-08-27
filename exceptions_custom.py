29b24d8d6a66578f6d5c792db672cfbb21c6d318
Vandanova Mariya <handarkin@gmail.com>
2022-08-27 21:32:05 +0800

    телеграм бот яп

--- exceptions_custom.py
+++ exceptions_custom.py
@@ -1,7 +1,3 @@
-class IncorrectFormatResponseError(Exception):
-    """"формат данных не соответствует требуемому"""
-    pass
-
 class EndpointError(Exception):
     """API не доступен по URL"""
     pass
